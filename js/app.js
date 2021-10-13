afd788e9bffe0b10464db1ec808657376c39e4fd
Andrew Ingombe <ingombe.ani@gmail.com>
2021-10-13 10:34:13 +0200

    Finish weather application

--- js/app.js
+++ js/app.js
@@ -1,4 +1,41 @@
 const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
+
+const updateUI = (data) => {
+  const cityDetail = data.cityDetails;
+  const weather = data.weather;
+
+  //   update detail template
+  details.innerHTML = `
+    <h5 class="my-3">${cityDetail.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+    </div>
+  `;
+
+  // upadte day and night images
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute("src", iconSrc);
+
+  let timeSrc = null;
+  if (weather.IsDayTime) {
+    timeSrc = "img/day.svg";
+  } else {
+    timeSrc = "img/night.svg";
+  }
+
+  time.setAttribute("src", timeSrc);
+
+  //remove display none class
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+};
 
 const updateCity = async (city) => {
   const cityDetails = await getCity(city);
@@ -18,7 +55,16 @@ cityForm.addEventListener("submit", (e) => {
 
   updateCity(city)
     .then((data) => {
-      console.log(data);
+      updateUI(data);
     })
     .catch((err) => console.log(err));
+
+  // set local storage
+  localStorage.setItem("city", city);
 });
+
+if (localStorage.getItem("city")) {
+  updateCity(localStorage.getItem("city"))
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
+}
