9ef0af2ad085fa229f52570f23506abc5a3dcaa8
Andrew Ingombe <ingombe.ani@gmail.com>
2021-10-16 11:13:40 +0200

    Update api end point

--- js/forecast.js
+++ js/forecast.js
@@ -2,7 +2,7 @@ const key = "gcIstBj7InS7OKU0OKD1QXgVbxZcPPMR"; //free practice account
 
 // get weather information
 const getWeather = async (id) => {
-  const baseUrl = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const baseUrl = "https://dataservice.accuweather.com/currentconditions/v1/";
   const query = `${id}?apikey=${key}`;
 
   const response = await fetch(baseUrl + query);
@@ -14,7 +14,7 @@ const getWeather = async (id) => {
 // get city information
 const getCity = async (city) => {
   const baseUrl =
-    "http://dataservice.accuweather.com/locations/v1/cities/search";
+    "https://dataservice.accuweather.com/locations/v1/cities/search";
   const query = `?apikey=${key}&q=${city}`;
 
   const response = await fetch(baseUrl + query);
